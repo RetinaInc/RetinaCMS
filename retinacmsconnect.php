ee673140ddf9e4b86354edfcee6ff1edc39d5a66
Rabin Awal <morbid_leathal@hotmail.com>
2013-03-29 15:24:07 +0545

    Update retinacmsconnect.php
    Just updated the database connection file. I have made my mind recently to do it Object Oriented way and use PDO.

--- retinacmsconnect.php
+++ retinacmsconnect.php
@@ -1,16 +1,32 @@
 <?php
-//database connection parameters declaration
+class dbconnect
+{
+  //database connection parameters declaration
+  // for more www.retina.net.np 
+  protected $dbconnect;
+  protected $appname = "RetinaCMS"; //to pull dynamically web application name
+  private $baseUrl = $_SERVER['HTTP_HOST'];
+	private $host = "localhost";// change as yours
+	private $username = "root"; // change as yours
+	private $password = "password1"; // change as yours
+	private $dbname = "retinainc";
+	private $database="mysql";
 
-// for more www.retina.net.np 
-$dbhost = 'localhost';// change as yours
-$dbname = 'retinacms';
-$dbuser = 'root';// change as yours
-$dbpass = 'password';// change as yours
-
-$appname = "RetinaCMS"; //to pull dynamically web application name
-$baseUrl = $_SERVER['HTTP_HOST'];
-//connection
-mysql_connect($dbhost, $dbuser, $dbpass) or die (mysql_error());
-mysql_select_db($dbname) or die (mysql_error());
-
+	function __construct()
+	{
+		if ($this->database == 'mysql') 
+		{
+			try
+			{
+				$this->dbconnect = new PDO ("mysql:host = $this->host; dbname = $this->dbname", $this->username, $this->password);
+			}
+			catch(PDOException $e)
+			// $e defined for exception object
+			{
+				die('Error Connection Database: ' .$e->getMessage());
+			}
+		}
+		
+	}
+}
 ?>
