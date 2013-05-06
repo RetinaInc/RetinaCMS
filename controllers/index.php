be160b00f3cbc8f708a8567976cac7d924e20efd
Rabin Awal <morbid_leathal@hotmail.com>
2013-05-06 12:01:13 +0545

    Update index.php

--- controllers/index.php
+++ controllers/index.php
@@ -1,7 +1,27 @@
 <?php
-class Index
+
+/**
+* 
+*/
+class Index extends Controller
 {
+  
+	function __construct()
+	{
+		Controller::__construct();
+		echo '<br>This is index in controller Folder.';
 
+		//$this->view->render('index/index');
+
+	}
+	function Index()
+	{
+		echo '<br>Index Index.';
+		$this->view->render('index/index');
+	}
+	function details()
+	{
+		
+		$this->view->render('index/index');
+	}
 }
-
-?>
