078f89875c92aafb0722ae2cd4ab7eac18105b67
Rabin Awal <morbid_leathal@hotmail.com>
2013-05-06 14:37:00 +0545

    Create login_model.php

---
+++ models/login_model.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+/**
+* 
+*/
+class Login_Model extends Model
+{
+  
+	function __construct()
+	{
+		//Controller::__construct();
+		echo '<br>This is help in Models Folder.';
+
+	}
+
+	public function other()
+	{
+		echo 'This is other in help controller Folder.';
+	}
+}
+?>
