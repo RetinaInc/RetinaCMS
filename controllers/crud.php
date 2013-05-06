0b1d3a1daa0d37459290f2e83714d186d456d431
Rabin Awal <morbid_leathal@hotmail.com>
2013-05-06 14:27:08 +0545

    Create crud.php

---
+++ controllers/crud.php
@@ -0,0 +1,40 @@
+class Crud extends Controller
+{
+  
+	function __construct()
+	{
+		Controller::__construct();
+		echo '<br>This is index in controller Folder.';
+
+		//$this->view->render('login/index');
+
+		
+
+	}
+	function index()
+	{
+		//require 'models/signup_model.php';
+		//$model = new signup_model();
+		$this->view->render('blog/index');
+	}
+
+	function insert()
+	{
+
+	}
+
+	function select()
+	{
+
+	}
+
+	function delete()
+	{
+		
+	}
+
+	function delete()
+	{
+
+	}
+}
